9c0aa15c8a286488124569fc46df784ec4f69d06
Fernando <fernando@ucard.com>
2019-06-29 14:41:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, '/dist')))
-  .get('/', (req, res) => res.render('/dist/index.html'))
+  .use(express.static(path.join(__dirname, '/dist/ExtratoPublico')))
+  .get('/', (req, res) => res.render('/dist/ExtratoPublico/index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
